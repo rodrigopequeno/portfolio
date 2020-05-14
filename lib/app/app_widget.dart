f5c3b6e21235e18f04d03c09d42d3714fa9d2e27
Rodrigo Pequeno <rodrigo.pequeno6@gmail.com>
2020-05-14 13:48:43 -0300

    add theme

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -9,7 +9,30 @@ class AppWidget extends StatelessWidget {
       navigatorKey: Modular.navigatorKey,
       title: 'Rodrigo Pequeno',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.blue[900],
+        secondaryHeaderColor: Colors.blue[100],
+        iconTheme: IconThemeData(color: Colors.white),
+        textTheme: TextTheme(
+          headline1: TextStyle(color: Colors.white, fontFamily: 'Patua One'),
+          headline2: TextStyle(color: Colors.white, fontFamily: 'Patua One'),
+          headline5: TextStyle(
+            color: Colors.blue[900],
+            fontFamily: 'Patua One',
+            fontSize: 50,
+          ),
+          subtitle1: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Baloo 2',
+            fontSize: 30,
+          ),
+          subtitle2: TextStyle(
+            color: Colors.blue[900],
+            fontFamily: 'Patua One',
+            fontSize: 30,
+          ),
+          bodyText2: TextStyle(
+              color: Colors.black, fontFamily: 'Baloo 2', fontSize: 40),
+        ),
       ),
       initialRoute: '/',
       onGenerateRoute: Modular.generateRoute,
