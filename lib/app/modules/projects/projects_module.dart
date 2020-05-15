2405eb07b4e515710aab3a8bf43d2da8024332f2
Rodrigo Pequeno <rodrigo.pequeno6@gmail.com>
2020-05-15 19:53:02 -0300

    remove information controller

--- lib/app/modules/projects/projects_module.dart
+++ lib/app/modules/projects/projects_module.dart
@@ -2,12 +2,10 @@ import 'package:flutter_modular/flutter_modular.dart';
 
 import 'projects_controller.dart';
 import 'projects_page.dart';
-import 'widgets/information/information_controller.dart';
 
 class ProjectsModule extends ChildModule {
   @override
   List<Bind> get binds => [
-        Bind((i) => InformationController()),
         Bind((i) => ProjectsController()),
       ];
 
