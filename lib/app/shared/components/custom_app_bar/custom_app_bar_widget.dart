641f2bb3d4635115448e507d25f48961337c3fc1
Rodrigo Pequeno <rodrigo.pequeno6@gmail.com>
2020-05-15 17:25:46 -0300

    add buttons languages

--- lib/app/shared/components/custom_app_bar/custom_app_bar_widget.dart
+++ lib/app/shared/components/custom_app_bar/custom_app_bar_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:i18n_extension/i18n_widget.dart';
 import '../../utils/translation/universal_translation.i18n.dart';
 
 import 'custom_app_bar_controller.dart';
@@ -19,65 +20,94 @@ class CustomAppBarWidget extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            FittedBox(
-              fit: BoxFit.scaleDown,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20),
-                child: Text(
-                  "RP",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 60,
-                    fontFamily: 'Exotc350 Bd BT',
+            InkWell(
+              onTap: controller.goAbout,
+              child: FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text(
+                    "RP",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 60,
+                      fontFamily: 'Exotc350 Bd BT',
+                    ),
                   ),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(right: 70),
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    reverse: true,
-                    children: <Widget>[
-                      Center(
+            Container(
+              width: MediaQuery.of(context).size.width * 0.5,
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  reverse: true,
+                  children: <Widget>[
+                    _languages(context, textTheme),
+                    Center(
+                      child: InkWell(
+                        onTap: controller.goContact,
+                        child: Text(
+                          'contact'.i18n,
+                          style: textTheme,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 35),
                         child: InkWell(
-                          onTap: controller.goContact,
+                          onTap: controller.goPortfolio,
                           child: Text(
-                            'contact'.i18n,
+                            'portfolio'.i18n,
                             style: textTheme,
                           ),
                         ),
                       ),
-                      Center(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 35),
-                          child: InkWell(
-                            onTap: controller.goPortfolio,
-                            child: Text(
-                              'portfolio'.i18n,
-                              style: textTheme,
-                            ),
-                          ),
+                    ),
+                    Center(
+                      child: InkWell(
+                        onTap: controller.goAbout,
+                        child: Text(
+                          'about'.i18n,
+                          style: textTheme,
                         ),
                       ),
-                      Center(
-                        child: InkWell(
-                          onTap: controller.goAbout,
-                          child: Text(
-                            'about'.i18n,
-                            style: textTheme,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _languages(BuildContext context, TextStyle textTheme) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+        width: 80,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            InkWell(
+              onTap: () => I18n.of(context).locale = Locale("pt", "BR"),
+              child: Image.asset(
+                'assets/images/br.png',
+                width: 30,
+              ),
+            ),
+            InkWell(
+              onTap: () => I18n.of(context).locale = Locale("en", "US"),
+              child: Image.asset(
+                'assets/images/en.png',
+                width: 30,
+              ),
+            ),
           ],
         ),
       ),
